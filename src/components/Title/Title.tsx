c1c6a897b02b68ddf199f36b668741e717c4e279
Sertavr <pampuh.net@gmail.com>
2026-01-22 08:42:27 +0200

    deleted optional chaining operators in Title component

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -9,7 +9,7 @@ export const Title: React.FC<Props> = ({ user }) => {
   return (
     <h1 className="title" data-cy="title">
       {user
-        ? `${user?.name} (${user?.born} - ${user?.died})`
+        ? `${user.name} (${user.born} - ${user.died})`
         : 'No selected person'}
     </h1>
   );
